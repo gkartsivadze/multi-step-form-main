3a983208c172eade2affd64d5f688d9f967a6dbf
Giorgi Kartsivadze <105109104+gkartsivadze@users.noreply.github.com>
2023-05-09 19:06:43 +0400

    step-3

--- script.js
+++ script.js
@@ -13,10 +13,15 @@ const step = {
         $(".sub-cont").css("display", "none");
         $("#step-3").css("display", "flex");
         $("#rad_3").prop("checked", "true");
+    },
+    four : function() {
+        $(".sub-cont").css("display", "none");
+        $("#step-4").css("display", "flex");
+        $("#rad_4").prop("checked", "true");
     }
 }
 
-step.two();
+step.three();
 
 $("#rad_1").on("click", () => {
     step.one();
@@ -26,6 +31,9 @@ $("#rad_2").on("click", () => {
 })
 $("#rad_3").on("click", () => {
     step.three();
+})
+$("#rad_4").on("click", () => {
+    step.four();
 })
 
 $("#step-1 > input[type=button]").on("click", () => {
@@ -37,3 +45,27 @@ $("#step-2  .back.btn").on("click", () => {
 $("#step-2  .next.btn").on("click", () => {
     step.three();
 })
+$("#step-3  .back.btn").on("click", () => {
+    step.two();
+})
+$("#step-3  .next.btn").on("click", () => {
+    step.four();
+})
+
+$("#month-year-check").on("change", function() {
+    bonusSystem(this);
+})
+
+function bonusSystem(elem) {
+    if($(elem).prop("checked") == true) {
+        $(".card").append("<p class='bonus'>2 months free");
+        $(".price").each(function(i, element) {
+            $(element).text("$" + ($(element).text().match(/[0-9]+/) * 10) + "/ye");
+        })
+    } else if ($(elem).prop("checked") == false) {
+        $(".price").each(function(i, element) {
+            $(element).text("$" + ($(element).text().match(/[0-9]+/) / 10) + "/mo");
+        })
+        $(".bonus").remove();
+    }
+}
